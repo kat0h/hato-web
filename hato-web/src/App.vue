55e8d36a1c28f9c046f7343f00f9dedfdd83a404
KotaKato <katokota1102@icloud.com>
2020-09-29 22:59:46 +0900

    vue-cliで書き直した。タブUIを実装した

---
+++ hato-web/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- ヘッダ -->
+  <header>
+    <a href="index.html">
+      <img src="./assets/logo.svg"/>
+    </a>
+  </header>
+  <TabUI/>
+  <!-- フッター -->
+  <footer>
+    <h6>
+      Copyright︎ 2020 KotaKato. All rights reserved.
+    </h6>
+  </footer>
+</template>
+
+<script>
+import TabUI from './components/tabUIComp'
+export default {
+  name: 'App',
+  components: {
+    'TabUI': TabUI
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+header {
+  text-align: center;
+}
+header a img {
+  width: 100px;
+}
+</style>
---
+++ hato-web/src/components/tabUIComp.vue
@@ -0,0 +1,133 @@
+<template>
+    <div id="tabui">
+        <ul class="tabButtonBox">
+            <li>
+                <button
+                class="tabButton" v-on:click="selArticle('home')"
+                v-bind:class="{'tabActive': isTabActive == 'home'}">
+                    ホーム
+                </button>
+            </li>
+            <li>
+                <button
+                class="tabButton" v-on:click="selArticle('stamp')"
+                v-bind:class="{'tabActive': isTabActive == 'stamp'}" >
+                    スタンプ
+                </button>
+            </li>
+            <li>
+                <button
+                class="tabButton" v-on:click="selArticle('class')"
+                v-bind:class="{'tabActive': isTabActive == 'class'}">
+                    クラス展
+                </button>
+            </li>
+            <li>
+                <button
+                class="tabButton" v-on:click="selArticle('sell')"
+                v-bind:class="{'tabActive': isTabActive == 'sell'}">
+                    販売
+                </button>
+            </li>
+            <li>
+                <button
+                class="tabButton" v-on:click="selArticle('schedule')"
+                v-bind:class="{'tabActive': isTabActive == 'schedule'}" >
+                    スケジュール
+                </button>
+            </li>
+        </ul>
+        <ul class="tabArticle">
+            <li class="article" v-show="isTabActive === 'home'">
+                HOME
+            </li>
+            <li class="article" v-show="isTabActive === 'stamp'">
+                STAMP
+            </li>
+            <li class="article" v-show="isTabActive === 'class'">
+                CLASS
+            </li>
+            <li class="article" v-show="isTabActive === 'sell'">
+                SELL
+            </li>
+            <li class="article" v-show="isTabActive === 'schedule'">
+                SCHEDULE
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tabUI',
+    data(){
+    return {
+        isTabActive: 'home'
+    }
+    },
+    methods: {
+        selArticle: function(tabName){
+            this.isTabActive = tabName
+            console.log(this.isTabActive)
+        }
+    }
+}
+</script>
+
+<style>
+#tabui {
+font-family: Avenir, Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: center;
+color: #2c3e50;
+margin-top: 10px;
+}
+ul {
+padding: 0;
+margin: 0;
+}
+li {
+list-style: none;
+margin: none;
+}
+.tabButtonBox {
+display: flex;
+position: sticky;
+position: -webkit-sticky;
+top: 0px;
+overflow: auto;
+white-space: nowrap;
+background: #fff;
+}
+.tabButtonBox::-webkit-scrollbar {
+display: none;
+}
+.tabButton {
+position: relative;
+display: inline-block;
+padding: 8px 10px 5px 10px;
+height: 50px;
+width: 100px;
+text-decoration: none;
+color: #44617b;
+background: #fff;
+border-bottom: solid 4px #44617b;
+border: none;
+border-radius: 15px 15px 0 0;
+transition: .4s;
+font-weight: bold;
+margin: 10px 0px 0px 0px;
+}
+.tabActive {
+background: #44617b;
+color: #fff;
+}
+.tabButton:focus {
+outline: none;
+}
+.article {
+color: #FFF;
+background: #44617b;
+}
+</style>
